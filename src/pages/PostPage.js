cd419ca9f6fba66f4ea05a9a27764c022047dae3
alalshow <alalshow@hanmail.net>
2019-03-09 17:21:05 +0900

    123

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,10 +1,8 @@
 import React from 'react';
-const PostPage = () => {
-    return (
-        <div>
-            Post
-        </div>
-    )
-}
-
+import PageTemplate from 'components/common/PageTemplate';
+import PostInfo from 'components/post/PostInfo';
+import PostBody from 'components/post/PostBody';
+const PostPage = () => {  
+    return (  <PageTemplate> <PostInfo/><PostBody/> </PageTemplate> );
+};
 export default PostPage;
